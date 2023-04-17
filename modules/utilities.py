9e92ed8b8ef99b6858e7cf73596b2c9d033d76c4
Caio Ribeiro <adv_caioribeiro@outlook.com>
2023-04-17 12:47:04 -0300

    Added JSON for classes

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,5 +1,6 @@
+import json
 from os import system
-from colorama import init, Style, Fore
+from colorama import init, Fore
 init()
 
 
@@ -26,7 +27,6 @@ def main_menu():
         option = input('>>')
         option = option.casefold()
         if option == options[5].casefold():  # Exit
-            bye()
             break
         elif option == options[0].casefold():  # Create a character
             char_creation_menu()
@@ -62,14 +62,11 @@ def main_menu():
 def char_creation_menu():
     while True:
         clear()
-        print(Fore.MAGENTA+'CHARACTER CREATION MENU'+Fore.RESET)
-        print('Bard')
-        print('Cleric')
-        print('Rogue')
-        print('Warrior')
-        print('Wizard')
+        print(Fore.MAGENTA+'CHARACTER CREATION MENU'+Fore.RESET, end='\n\n')
+        for c in classes.keys():
+            print(' '*3+Fore.LIGHTCYAN_EX+c+Fore.RESET)
         print('Choose a class or')
-        print('Return to main menu')
+        print(Fore.LIGHTCYAN_EX+'Return'+Fore.RESET, 'to main menu')
         option = input('>>')
         if option.casefold() == 'Return'.casefold():
             break
@@ -84,3 +81,7 @@ def char_list_menu():
         option = input('>>')
         if option.casefold() == 'Return'.casefold():
             break
+
+
+with open('modules/classes.json') as f:
+    classes = json.load(f)
