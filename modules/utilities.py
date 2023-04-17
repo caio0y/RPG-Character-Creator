f2342f735a89abc6855faf440f00f02c28543838
Caio Ribeiro <adv_caioribeiro@outlook.com>
2023-04-17 20:50:43 -0300

    Update character creator menu

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,6 +1,6 @@
 import json
-from time import sleep
 from os import system
+from time import sleep
 from colorama import init, Fore
 init()
 
@@ -10,17 +10,17 @@ def clear():
 
 
 def main_menu():
-    menu = ({'Create': ' a character', 'List': ' all characters', 'Edit': ' character', 'Add': ' new class',
-            'Modify': ' existing class', 'Exit': ''})
+    menu = ({'Create': ' a character', 'List': ' all characters', 'Edit': ' existing character', 'Add': ' new class',
+             'Modify': ' existing class', 'Exit': ''})
     options = tuple(key.casefold() for key in menu.keys())
-    padding = ' '*3
+    padding = ' ' * 3
     while True:
         clear()
         print(Fore.MAGENTA + 'RPG CHARACTER CREATOR'.center(25))
         print('MAIN MENU'.center(25) + Fore.RESET, end='\n\n')
         for k, v in menu.items():
             sleep(0.1)
-            print(padding+Fore.LIGHTCYAN_EX+k+Fore.RESET+v)
+            print(padding + Fore.LIGHTCYAN_EX + k + Fore.RESET + v)
         option = select_validation(options)
         if option == options[5]:  # Exit
             break
@@ -46,7 +46,6 @@ def main_menu():
             input('>>')
             continue
         else:
-            input('Erro desconhecido')
             continue
 
 
@@ -56,19 +55,23 @@ def char_creation_menu():
     padding = ' ' * 3
     while True:
         clear()
-        print(Fore.MAGENTA+'CHARACTER CREATION MENU'+Fore.RESET, end='\n\n')
+        print(Fore.MAGENTA + 'CHARACTER CREATION MENU' + Fore.RESET, end='\n\n')
         for o in options:
             sleep(0.1)
-            print(padding+Fore.LIGHTCYAN_EX+o.title()+Fore.RESET)
+            print(padding + Fore.LIGHTCYAN_EX + o.title() + Fore.RESET)
         print('Choose a class or')
-        print(Fore.LIGHTCYAN_EX+'Return'+Fore.RESET, 'to main menu')
+        print(Fore.LIGHTCYAN_EX + 'Return' + Fore.RESET, 'to main menu')
         option = select_validation(options, True, exit_menu)
         if option == exit_menu.casefold():
             break
         elif option in options:
-            print(f'You choose {option.title()}')
-            sleep(1)
-            continue
+            confirm = show_class(option.title())
+            if confirm == 'Y':
+                input('Yes')
+                continue
+            else:
+                input('No')
+                continue
         else:
             continue
 
@@ -77,7 +80,7 @@ def char_list_menu():
     options = ['Return']
     while True:
         clear()
-        print('  Name | Class | Level\n'*5)
+        print('  Name | Class | Level\n' * 5)
         print('Choose a character or')
         print('Return to main menu')
         option = input('>>')
@@ -86,23 +89,40 @@ def char_list_menu():
 
 
 def select_validation(options_tuple, need_add_options=False, *add_options):
-    selected_option = input('>>').casefold()
-    options_list = [item.casefold() for item in options_tuple]
+    option = input('>>').casefold()
+    options = [item.casefold() for item in options_tuple]
     if need_add_options:
-        options_list.extend([item.casefold() for item in add_options])
-    if selected_option not in options_list:
+        options.extend([item.casefold() for item in add_options])
+    if option not in options:
         print('\n' + Fore.LIGHTBLACK_EX + 'Options: ')
-        for i in range(0, len(options_list)):
-            print(f'[{options_list[i].title()}]', end='')
+        for i in range(0, len(options)):
+            print(f'[{options[i].title()}]', end='')
         print('\n' + Fore.RESET)
-        selected_option = input('>>').casefold()
-        while selected_option not in options_list:
-            print(Fore.RED+'Invalid option!'+Fore.RESET)
-            selected_option = input('>>').casefold()
-        return selected_option
+        option = input('>>').casefold()
+        while option not in options:
+            print(Fore.RED + 'Invalid option!' + Fore.RESET)
+            option = input('>>').casefold()
+        return option
     else:
-        return selected_option
+        return option
 
 
-with open('modules/classes.json') as f:
+def show_class(choose_class):
+    class_data = classes[choose_class]
+    skills = class_data['skills']
+    print(Fore.LIGHTMAGENTA_EX + class_data['name'] + Fore.RESET)
+    print(class_data['description'])
+    for k, v in class_data['modify'].items():
+        if v > 0:
+            print(f'+{v} {k}')
+    print('')
+    for skill in skills:
+        print(skill['name'])
+        print(skill['description'])
+        print('')
+    create = input('Confirm selection? [Y]') or 'Y'
+    return create
+
+
+with open('database/classes.json') as f:
     classes = json.load(f)
